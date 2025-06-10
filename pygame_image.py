ffe0d3d9a5a1bd1f146700b589836f6d3bfe74c7
c0a24136de <c0a24136de@edu.teu.ac.jp>
2025-06-10 15:25:41 +0900

    練習3

---
+++ pygame_image.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像のsurface
+    bg_img2 = pg.image.load("fig/3.png")
+    # bg_img2 = pg.transform.flip(bg_img2,True,False)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        # screen.blit(bg_img2,[300,200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
